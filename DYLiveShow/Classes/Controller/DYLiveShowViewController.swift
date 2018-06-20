0d78348538796671c88138a8e046997b4536467e
胡鹏 <64hp@163.com>
2018-06-20 13:35:37 +0800

    fix

--- DYLiveShow/Classes/Controller/DYLiveShowViewController.swift
+++ DYLiveShow/Classes/Controller/DYLiveShowViewController.swift
@@ -13,9 +13,9 @@ class DYLiveShowViewController: NJViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "直播"
-        nj_isBackActionBtnHidden = true
+//        nj_isBackActionBtnHidden = true
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.navigationController?.pushViewController(NJViewController(), animated: true)
+
     }
 }
