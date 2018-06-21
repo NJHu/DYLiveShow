541b9623e47acb26a90e3698eaebdb82ef9966bc
胡鹏 <64hp@163.com>
2018-06-21 18:27:51 +0800

    update

--- DYLiveShow/Classes/Controller/DYLiveShowViewController.swift
+++ DYLiveShow/Classes/Controller/DYLiveShowViewController.swift
@@ -7,15 +7,18 @@
 
 import UIKit
 import NJKit
+import NJDYSearchBarView
+import SnapKit
 
 class DYLiveShowViewController: NJViewController {
-
+    
+    private let searchBarView = NJDYSearchBarView.searchBarView()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "直播"
-//        nj_isBackActionBtnHidden = true
-    }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        nj_isBackActionBtnHidden = true
+        nj_navigationBar.isHidden = true
+        view.addSubview(searchBarView)
+        searchBarView.makeco
     }
 }
