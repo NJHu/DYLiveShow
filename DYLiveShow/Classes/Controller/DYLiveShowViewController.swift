b690ed6337504f0ff0b441986db4205c14cc75fd
胡鹏 <64hp@163.com>
2018-06-21 19:10:05 +0800

    add

--- DYLiveShow/Classes/Controller/DYLiveShowViewController.swift
+++ DYLiveShow/Classes/Controller/DYLiveShowViewController.swift
@@ -11,14 +11,24 @@ import NJDYSearchBarView
 import SnapKit
 
 class DYLiveShowViewController: NJViewController {
-    
+    let headerView = UIView()
     private let searchBarView = NJDYSearchBarView.searchBarView()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "直播"
         nj_isBackActionBtnHidden = true
         nj_navigationBar.isHidden = true
-        view.addSubview(searchBarView)
-//        searchBarView.makeco
+        view.addSubview(headerView)
+        headerView.snp.makeConstraints { (make) in
+            make.top.left.right.equalToSuperview()
+            make.height.equalTo(88 + UIApplication.shared.statusBarFrame.height)
+        }
+        headerView.backgroundColor = UIColor.orange
+        headerView.addSubview(searchBarView)
+        searchBarView.snp.makeConstraints { (maker) in
+            maker.left.right.equalToSuperview()
+            maker.height.equalTo(44)
+            maker.top.equalToSuperview().offset(UIApplication.shared.statusBarFrame.height)
+        }
     }
 }
